626a38825bd188e9259b7ad994c411499be2e16f
root <a@b.c>
2021-12-03 06:39:50 +0000

    c.cpp

--- aoc03/c.cpp
+++ aoc03/c.cpp
@@ -6,52 +6,18 @@
 
 using namespace std;
 
-int conv(string s){
-  int y = 0;
-  int t = 1;
-  for(int i = s.size()-1; i >= 0; i--){
-    if(s[i] == '1'){
-      y += t;
-    }
-    t *= 2;
-  }
-  return y;
-}
-
-string n(string s){
-  string o = "";
-  for(char c : s){
-    o = o + ((c == '1')?'0':'1');
-  }
-  return o;
-}
-
-string o(vector<string> v, int s){
+string o(vector<string> v, int s, bool (*f)(int)){
   int x = 0;
   for(string a : v){
     x += (a[s] == '1')?1:-1;
   }
   vector<string> t;
-  char c = (x >= 0)? '1':'0';
+  char c = (f(x))? '1':'0';
   for(string a : v){
     if(a[s] == c) t.push_back(a);
   }
   if(t.size() == 1) return t[0];
-  return o(t,s+1);
-}
-
-string co(vector<string> v, int s){
-  int x = 0;
-  for(string a : v){
-    x += (a[s] == '1')?1:-1;
-  }
-  vector<string> t;
-  char c = (x >= 0)? '0':'1';
-  for(string a : v){
-    if(a[s] == c) t.push_back(a);
-  }
-  if(t.size() == 1) return t[0];
-  return co(t,s+1);
+  return o(t,s+1,f);
 }
 
 int main(){
@@ -73,9 +39,9 @@ int main(){
     s << (res[i] > 0)?1:0;
   }
 
-  cout << conv(s.str()) * conv(n(s.str())) << endl;
+  cout << [](int x){ return x * (4095 - x); } (stoi(s.str(),0,2)) << endl;
 
-  cout << conv(o(vals,0)) * conv(co(vals,0)) << endl;
+  cout << stoi(o(vals,0,[](int x){ return x >= 0;}),0,2) * stoi(o(vals,0,[](int x){ return x < 0;}),0,2) << endl;
   
   return 0;
 }
