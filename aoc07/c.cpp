7112f99e97849ff632f6bbcac2a874a1ef4f6163
root <a@b.c>
2021-12-07 11:06:45 +0000

    day 7 updates

--- aoc07/c.cpp
+++ aoc07/c.cpp
@@ -1,52 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <sstream>
+#include <algorithm>
+#include <cmath>
+#include <regex>
+#include <numeric>
 
 using namespace std;
-
-int abs(int i){
-  return (i < 0)? -i:i;
-}
-
-long long int tri(int i){
-  long long int o = 0;
-  for(i = abs(i); i >0; i--) o+= i;
-  return o; 
-}
 
 int main(){
   string l;
   ifstream f("input");
-  vector<int> c;
+
   getline(f,l);
-  for(int i = 0; i < l.size(); i++) if(l[i] == ',') l[i] = ' ';
 
-  stringstream s(l);
+  vector<int> c;
+  regex rgx (",");
+  regex_token_iterator<string::iterator> it(l.begin(), l.end(), rgx, -1);
+  regex_token_iterator<string::iterator> end;
+  for(; it != end; ++it) c.push_back(stoi(*it));
 
-  int x;
-  
-  while(s>>x) c.push_back(x);
+  sort(c.begin(), c.end());
+  int median = c[c.size()/2];
 
-  int q = 10000000;
-  long long int q2 = 10000000000000;
+  cout << accumulate(c.begin(),c.end(),0,[median](int x, int y){return x + abs(y-median);}) << endl;
 
+  int mean = accumulate(c.begin(),c.end(),0)/c.size();
 
-  for(int o = 0; o < 1000; o++){
-
-    int out = 0;
-    long long int out2 = 0;
-
-    for(int i : c) out += abs(i-o);
-    for(int i : c) out2 += tri(i-o);
-
-    if(out < q) q = out;
-    if(out2 < q2) q2 = out2;
-  }
-
-  cout << q << endl;
-  cout << q2 << endl;
-
+  cout << accumulate(c.begin(),c.end(),0,[mean](int x, int y){return x + [](int t){return t*(t+1)/2;}(abs(y-mean));}) << endl;
 
   return 0;
 }
