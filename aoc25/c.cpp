6821b3d302dc18236ca6317955bc0334b8d16b52
root <a@b.c>
2021-12-25 05:43:03 +0000

    clean up day 25

--- aoc25/c.cpp
+++ aoc25/c.cpp
@@ -26,23 +26,9 @@ map<pair<int,int>,char> move(map<pair<int,int>,char> q, int s, int g, char c){
   return o;
 }
 
-
 map<pair<int,int>,char> sim(map<pair<int,int>,char> q, int s, int g){
   map<pair<int,int>,char> o = move(q, s, g, '>');
   return move(o, s, g, 'v');
-}
-
-void print(map<pair<int,int>,char> q){
-  for(int i = 0; i < 9; i++){
-    for(int j = 0; j < 10; j++){
-      if(q.count(pair<int,int>(i,j))){
-        cout << q[pair<int,int>(i,j)];
-      } else {
-        cout << '.';
-      }
-    }
-    cout << endl;
-  }
 }
 
 int main(){
@@ -50,19 +36,15 @@ int main(){
   ifstream f("input");
   map<pair<int,int>,char> q;
 
-
   int i = 0;
   while(getline(f,l)){
     for(int j = 0; j < l.size(); j++) if(l[j] != '.') q.insert(pair<pair<int,int>,char>(pair<int,int>(i,j),l[j]));
     i++;
   }
-  print(q);
-  cout << endl;
 
   int g = i;
   i = 1;
   map<pair<int,int>,char> t = sim(q, 137, 139);
-  print(t);
   while(t != q){
     i++;
     q = t;
